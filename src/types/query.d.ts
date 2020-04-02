e2dc41312edce0f649f533a7b3f42d667d094f8f
Alec Larson <alec.stanford.larson@gmail.com>
2020-04-01 22:15:04 -0400

    types: shorten "q.IsEmpty" type signature

--- src/types/query.d.ts
+++ src/types/query.d.ts
@@ -164,7 +164,7 @@ export module query {
   export function Prepend(elements: ExprArg, collection: ExprArg): Expr
   export function Append(elements: ExprArg, collection: ExprArg): Expr
 
-  export function IsEmpty(collection: Expr.Iterable<any>): Expr<boolean>
+  export function IsEmpty(collection: Expr.Iterable): Expr<boolean>
 
   // TODO
   export function IsNonEmpty(collection: ExprArg): Expr
